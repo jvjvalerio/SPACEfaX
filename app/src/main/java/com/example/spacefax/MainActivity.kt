9b3d187e607a289a6f1abf2103805c6c460e4be3
jvjvalerio <julio.valerio2@live.lagcc.cuny.edu>
2022-03-11 13:56:03 -0500

    Add Apollo and GraphQL plugins along with dependencies Add separate Model-View-Presenter files for separation of concerns Add SpaceX GraphQL Schema.sdl

--- app/src/main/java/com/example/spacefax/MainActivity.kt
+++ app/src/main/java/com/example/spacefax/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.spacefax
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Contract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
