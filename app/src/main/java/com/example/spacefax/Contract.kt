5a9853dfeb349801ebb17185c04b568c3ab8c9e2
jvjvalerio <julio.valerio2@live.lagcc.cuny.edu>
2022-03-11 14:07:56 -0500

    Add Contract Model schema.sdl files

---
+++ app/src/main/java/com/example/spacefax/Contract.kt
@@ -0,0 +1,15 @@
+package com.example.spacefax
+
+interface Contract {
+    interface View {
+
+    }
+
+    interface Model {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ app/src/main/java/com/example/spacefax/Model.kt
@@ -0,0 +1,6 @@
+package com.example.spacefax
+
+class Model : Contract.Model {
+
+
+}
